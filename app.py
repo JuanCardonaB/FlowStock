b927b532487883cc53e99429d7fb8038cea8d0f3
JuanCardonaB <juanjocarbol@gmail.com>
2024-10-19 18:20:45 -0500

    cors

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from products.router.products_router import router as products_router
 from users_types.router.users_types_router import router as users_types_router
 
@@ -7,3 +8,11 @@ app = FastAPI()
 # add a route to the app
 app.include_router(products_router, prefix="/products")
 app.include_router(users_types_router, prefix="/users_types")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
