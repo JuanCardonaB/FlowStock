761c6f4dc424f45ce0c9f4c8266e2b7ebd6515e8
JuanCardonaB <juanjocarbol@gmail.com>
2024-10-19 01:27:12 -0500

    SQL setup

---
+++ db/models/FlowStockDB.py
@@ -0,0 +1,111 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class users_types(Base):
+    __tablename__ = 'users_types'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    created_at = Column(DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "created_at": self.created_at
+        }
+
+class users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False)
+    password = Column(String(50), nullable=False)
+    phone = Column(String(50), nullable=False)
+    address = Column(String(100), nullable=False)
+    user_type_id = Column(Integer, ForeignKey('users_types.id'), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "email": self.email,
+            "password": self.password,
+            "phone": self.phone,
+            "address": self.address,
+            "user_type_id": self.user_type_id,
+            "created_at": self.created_at
+        }
+
+class products(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(50), nullable=False)
+    description = Column(String(50), nullable=False)
+    price = Column(Float, nullable=False)
+    stock = Column(Integer, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "price": self.price,
+            "stock": self.stock,
+            "created_at": self.created_at
+        }
+    
+class sales(Base):
+    __tablename__ = 'sales'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    total = Column(Float, nullable=False)
+    sale_date = Column(DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "total": self.total,
+            "sale_date": self.sale_date
+        }
+
+class sales_details(Base):
+    __tablename__ = 'sales_details'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    sale_id = Column(Integer, ForeignKey('sales.id'), nullable=False)
+    product_id = Column(Integer, ForeignKey('products.id'), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    subtotal = Column(Float, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "sale_id": self.sale_id,
+            "product_id": self.product_id,
+            "quantity": self.quantity,
+            "subtotal": self.subtotal
+        }
+    
+class inventory(Base):
+    __tablename__ = 'inventory'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    product_id = Column(Integer, ForeignKey('products.id'), nullable=False)
+    supplier_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    quantity = Column(Integer, nullable=False)
+    created_at = Column(DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "product_id": self.product_id,
+            "quantity": self.quantity,
+            "supplier_id": self.supplier_id,
+            "created_at": self.created_at
+        }
